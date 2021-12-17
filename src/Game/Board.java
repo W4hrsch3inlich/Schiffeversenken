de051b026cbf03b1a2dd54cfe6656009dfa4e77a
Armin Wolf <W_Armin@gmx.de>
2021-12-17 15:56:27 +0100

    Init project structure.
    Signed-off-by: Armin Wolf <W_Armin@gmx.de>

---
+++ src/Game/Board.java
@@ -0,0 +1,21 @@
+package Game;
+
+public class Board {
+	private Ship[] ship;
+	private int xMax;
+	private int yMax;
+	
+	public Board(int x, int y, int num) {
+		this.xMax = x;
+		this.yMax = y;
+		this.ship = new Ship[num];
+	}
+	
+	public boolean check() {
+		
+	}
+	
+	public int shoot(int x, int y) {
+		
+	}
+}
