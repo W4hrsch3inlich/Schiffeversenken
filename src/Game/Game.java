e99406b6d9bc2201cbf72b6d7643757dde1da23a
Armin Wolf <W_Armin@gmx.de>
2022-01-07 14:33:54 +0100

    Game: Add javadoc comments
    They mainly provide a TODO for now.
    
    Signed-off-by: Armin Wolf <W_Armin@gmx.de>

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -1,10 +1,28 @@
 package Game;
 
+/**
+ * <h1>Schiffeversenken</h1>
+ * The Game allows to players to play "Schiffeversenken"
+ * over the command line. First, each player has to
+ * secretly enter the position of his Ships, then both
+ * player take turns shooting at the enemy Ships until
+ * one player runs out of Ships and looses.
+ *
+ * @author wolf
+ */
 public class Game {
-
+	/**
+	 * Main method, instantiates a Game object.
+	 * @param args Command line arguments, 1. Number of Ships for each Board.
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
-
+	/* TODO:
+	 * Populate the Game class with the necessary methods.
+	 * The Game class instantiates a two Boards with a given
+	 * number of Ships. Then both players take turns shooting
+	 * at the enemy Board until a Board runs out of Ships.
+	 */
 }
