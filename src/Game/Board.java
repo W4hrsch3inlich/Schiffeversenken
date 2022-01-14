13f95cea2794000aed1694d94083f420b2c2b075
Armin Wolf <W_Armin@gmx.de>
2022-01-14 08:14:18 +0100

    Board: Implement functionality
    Still needs some testing.
    
    Signed-off-by: Armin Wolf <W_Armin@gmx.de>
    Signed-off-by: Armin Wolf <robo-armin@gmx.de>

--- src/Game/Board.java
+++ src/Game/Board.java
@@ -1,4 +1,5 @@
 package Game;
+import java.util.Scanner;
 
 /**
  * The Board class contains the logic for
@@ -29,26 +30,143 @@ public class Board {
 	 * @param num Number of Ships.
 	 */
 	public Board(int x, int y, int num) {
+		/* Coordinates start in the lower left corner */
 		this.xMax = x;
 		this.yMax = y;
 		this.ship = new Ship[num];
-		/**
-		 * TODO:
-		 * Ask the player to enter the data necessary to instantiate
-		 * the Ships over the command line.
-		 */
+		int xCoord, yCoord, length, xDirection, yDirection;
+		boolean overlapps = false;
+
+		for (int i = 0; i < num; i++) {
+			while (true) {
+				/* clears the console */
+				System.out.print("\033[H\033[2J");
+				System.out.println("Ship number " + i + " :");
+
+				System.out.println("Starting point (X Y)?");
+				while (true) {
+					xCoord = this.getInt();
+					if (xCoord > this.xMax || xCoord < 0) {
+						System.out.println("The maximum value for X is " + this.xMax);
+						continue;
+					}
+					yCoord = this.getInt();
+					if (yCoord > this.yMax || yCoord < 0) {
+						System.out.println("The maximum value for Y is " + this.yMax);
+						continue;
+					}
+					break;
+				}
+
+				System.out.println("Length?");
+				while (true) {
+					length = this.getInt();
+					if (length > this.xMax - xCoord && length > this.yMax - yCoord) {
+						System.out.println("Ship would be too long");
+						continue;
+					}
+					break;
+				}
+
+				System.out.println("In which direction (N/E/S/W)?");
+				while (true) {
+					switch (this.getChar()) {
+					case 'N':
+						xDirection = 0;
+						yDirection = 1;
+						break;
+					case 'E':
+						xDirection = 1;
+						yDirection = 0;
+						break;
+					case 'S':
+						xDirection = 0;
+						yDirection = -1;
+						break;
+					case 'W':
+						xDirection = -1;
+						yDirection = 0;
+						break;
+					default:
+						System.out.println("Not a valid direction");
+						continue;
+					}
+					if (xCoord + length * xDirection > this.xMax || xCoord + length * xDirection > 0 ||
+						yCoord + length * yDirection > this.yMax || yCoord + length * yDirection > this.yMax) {
+						System.out.println("Ship would not fit onto this Board");
+						continue;
+					}
+					break;
+				}
+				for (Ship ship: this.ship) {
+					if (ship == null) {
+						continue;
+					}
+					for (int cell_num = 0; cell_num <= length; cell_num++) {
+						if (ship.check(xCoord + cell_num * xDirection, yCoord + cell_num * yDirection)) {
+							System.out.println("Ship is overlapping");
+							overlapps = true;
+						}
+					}
+				}
+				if (!overlapps) {
+					this.ship[i] = new Ship(xCoord, yCoord, length, xDirection, yDirection);
+					break;
+				}
+				/* The while loop retries the input after 1 second */
+				try {
+				    Thread.sleep(1000);
+				}
+				catch(InterruptedException ex) {
+				    Thread.currentThread().interrupt();
+				}
+			}
+		}
 	}
 
+	private int getInt() {
+		/* stdin is automatically closed on exit */
+		try (Scanner stdin = new Scanner(System.in)) {
+			while (true) {
+				try {
+					return stdin.nextInt();
+				} catch (java.util.InputMismatchException e) {
+					/* nextLine() is necessary to flush stdin! */
+					System.out.println(stdin.nextLine() + " is not a valid number!");
+					continue;
+				}
+			}
+		}
+	}
+
+	private char getChar() {
+		/* stdin is automatically closed on exit */
+		try (Scanner stdin = new Scanner(System.in)) {
+			while (true) {
+				String str = stdin.next();
+				if (str.length() > 1) {
+					System.out.println("To much info");
+					continue;
+				}
+
+				return str.charAt(0);
+			}
+		}
+	}
 	/**
 	 * Checks if a Board does still contain any Ships.
 	 * @return boolean True if no remaining Ships on the given Board
 	 */
 	public boolean check() {
-		/**
-		 * TODO:
-		 * Iterate through all Ships and check if all
-		 * of them are sunk with .check().
-		 */
+		boolean sunkAll = true;
+
+		for (Ship ship: this.ship) {
+			if (ship != null) {
+				sunkAll = false;
+			}
+		}
+
+		return sunkAll;
 	}
 
 	/**
@@ -58,10 +176,23 @@ public class Board {
 	 * @return int 0 = no hit, 1 = first hit, 2 = hit again, 3 = sunk a Ship.
 	 */
 	public int shoot(int x, int y) {
-		/**
-		 * TODO:
-		 * Iterate through all Ships and check if one
-		 * gets hit with .shoot().
-		 */
+		int result = 0;
+
+		for (Ship ship: this.ship) {
+			if (ship == null) {
+				/* Ship is already sunk */
+				continue;
+			}
+			result = ship.shot(x,  y);
+			if (result > 0) {
+				if (result == 3) {
+					/* mark Ship as sunken */
+					ship = null;
+				}
+				break;
+			}
+		}
+
+		return result;
 	}
 }
